ab8a2f1862cd0c36a52169eea70e2c1d5c60df13
yuvasri5 <60392326+yuvasri5@users.noreply.github.com>
2020-07-02 20:49:07 +0530

    Create Interest_cal.java

---
+++ Interest_cal.java
@@ -0,0 +1,8 @@
+public class Interest_cal {
+	public double Simple_Interest(double p,double r,double t){
+ 	   return (p*r*t)/100;
+    }
+    public double Compound_Interest(double p,double r,double t){
+ 	   return (p*Math.pow(1+(r/100),t));
+    }
+}
